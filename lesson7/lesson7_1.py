003fed167a1b4b0ba72ab54550ce3f52faa44c3a
Oakley0910 <a0987029247@gmail.com>
2024-12-14 06:10:11 +0000

    7-6

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,23 +1,4 @@
-import requests
-from requests import Response
-from io import StringIO
-from csv import DictReader
+import tools
 
-url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
-
-try:
-
-    r:Response = requests.request('GET',url)
-    r.raise_for_status()
-    print('下載成功')
-except HTTPError as e:
-    print(e)
-except RequestException as e:
-    print(e)
-
-else:
-    print("下載成功")
-    file = StringIO(r.text)
-    reader = DictReader(file)
-    list_reader:list[dict] = list(reader)
-    print(list_reader)
+youbike_data:list[dict] = tools.get_youbikes()
+print(youbike_data)
