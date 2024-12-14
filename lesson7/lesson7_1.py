26b62cad9b0f24ae192a62953d6c05e9acf7fa9a
Oakley0910 <a0987029247@gmail.com>
2024-12-14 06:00:47 +0000

    7-5

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,23 @@
+import requests
+from requests import Response
+from io import StringIO
+from csv import DictReader
+
+url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
+
+try:
+
+    r:Response = requests.request('GET',url)
+    r.raise_for_status()
+    print('下載成功')
+except HTTPError as e:
+    print(e)
+except RequestException as e:
+    print(e)
+
+else:
+    print("下載成功")
+    file = StringIO(r.text)
+    reader = DictReader(file)
+    list_reader:list[dict] = list(reader)
+    print(list_reader)
