17d8f59e19db0da2292f1b3939bc7f735699130a
Oakley0910 <a0987029247@gmail.com>
2024-12-21 06:25:52 +0000

    8-6

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -8,16 +8,22 @@ youbike_data:list[dict] = fetch_youbike_data()
 # 左邊是選擇行政區域(sarea), 使用下拉式表單
 # 右邊是顯示該行政區域的YouBike站點資訊的表格資料
 # 最下方是顯示該行政區域的YouBike站點資訊的地圖
-sarea_list = sorted(set(map(lambda item:item['sarea'],youbike_data)))
-col1,col2 = st.columns(2)
+area_list = list(set(map(lambda item:item['sarea'],youbike_data)))
+col1,col2 = st.columns([1,3])
 with col1:
-    selected_sarea = st.selectbox("行政區域",sarea_list)
+    selected_area = st.selectbox("行政區域", area_list)
 
 with col2:
-    filter_data = filter(lambda item:item['sarea'] == selected_sarea,youbike_data)
-    st.dataframe(filter_data)
+    st.write(selected_area)
 
-#顯示地圖
-filter_data = list(filter(lambda item:item['sarea'] == selected_sarea,youbike_data))
-locations = [{'lat': float(item['lat']), 'lon': float(item['lng'])} for item in filter_data]
-st.map(locations)
+# with col1:
+#     selected_sarea = st.selectbox("行政區域",sarea_list)
+
+# with col2:
+#     filter_data = filter(lambda item:item['sarea'] == selected_sarea,youbike_data)
+#     st.dataframe(filter_data)
+
+# #顯示地圖
+# filter_data = list(filter(lambda item:item['sarea'] == selected_sarea,youbike_data))
+# locations = [{'lat': float(item['lat']), 'lon': float(item['lng'])} for item in filter_data]
+# st.map(locations)
